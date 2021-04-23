e8ef7b9919c326d673ae2f839c749bbedeefa87f
hthoai <hthoai1006@gmail.com>
2021-04-23 11:18:39 +0000

    ADD ReplayBuffer

---
+++ utils/replay_buffer.py
@@ -0,0 +1,42 @@
+import random
+
+import torch
+
+
+class ReplayBuffer:
+    """An image buffer that stores previously generated images."""
+
+    def __init__(self, max_size: int=50) -> None:
+        assert (max_size > 0)
+        self.max_size = max_size
+        self.buffer = []
+    
+    def query(self, images):
+        # If the buffer size is 0, do nothing
+        if self.max_size == 0:
+            return images
+        return_images = []
+        for image in images:
+            image = torch.unsqueeze(image.data, 0)
+            # If the buffer is not full,
+            # keep inserting current images to the buffer
+            if self.num_imgs < self.pool_size:   
+                self.num_imgs = self.num_imgs + 1
+                self.buffer.append(image)
+                return_images.append(image)
+            else:
+                p = random.uniform(0, 1)
+                # The buffer will return a previously stored image,
+                # and insert the current image into the buffer
+                if p > 0.5:  
+                    random_id = random.randint(0, self.pool_size - 1)
+                    tmp = self.buffer[random_id].clone()
+                    self.buffer[random_id] = image
+                    return_images.append(tmp)
+                # The buffer will return the current image
+                else:
+                    return_images.append(image)
+        # Collect all the images and return
+        return_images = torch.cat(return_images, 0)
+        
+        return return_images
