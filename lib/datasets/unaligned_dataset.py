1af7f49c8b8bf42d793ec19195fa448427b0ece0
hthoai <hthoai1006@gmail.com>
2021-04-28 10:26:01 +0000

    MOD transforms

--- lib/datasets/unaligned_dataset.py
+++ lib/datasets/unaligned_dataset.py
@@ -4,59 +4,41 @@ import glob
 
 from PIL import Image
 import numpy as np
-import imgaug.augmenters as iaa
 from torch.functional import Tensor
 
 from torch.utils.data import Dataset
-from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
 
-IMAGENET_MEAN = np.array([0.485, 0.456, 0.406])
-IMAGENET_STD = np.array([0.229, 0.224, 0.225])
+IMAGENET_MEAN = np.array([0.5, 0.5, 0.5])  # np.array([0.485, 0.456, 0.406])
+IMAGENET_STD = np.array([0.5, 0.5, 0.5])  # np.array([0.229, 0.224, 0.225])
 
 
 class UnalignedDataset(Dataset):
     """Ref: https://github.com/Lornatang/CycleGAN-PyTorch"""
 
-    def __init__(
-        self,
-        root: str,
-        img_size: List,
-        normalize: bool = True,
-        transforms: Any = None,
-        transform_rate: float = 0.5,
-    ) -> None:
-        if transforms is not None:
-            transformations = [
-                getattr(iaa, trans["name"])(**trans["parameters"])
-                for trans in transforms
+    def __init__(self, root: str, img_size: List, transformations: Any = None) -> None:
+        transforms_list = [
+            transforms.ToTensor(),
+            transforms.Resize(size=img_size, interpolation=Image.BICUBIC),
+        ]
+        if transformations is not None:
+            transforms_list += [
+                getattr(transforms, trans["name"])(**trans["parameters"])
+                for trans in transformations
             ]
-        else:
-            self.transformations = []
-        self.normalize = normalize
-        self.transform = iaa.Sequential(
-            [
-                iaa.Sometimes(then_list=transformations, p=transform_rate),
-                iaa.Resize({"height": img_size[0], "width": img_size[1]}),
-            ]
-        )
-        self.to_tensor = ToTensor()
+        self.transform = transforms.Compose(transforms_list)
         self.files_A = sorted(glob.glob(root + "A/*.*"))
         self.files_B = sorted(glob.glob(root + "B/*.*"))
 
     def __getitem__(self, index: int) -> Tuple[Tensor, Tensor]:
         # Get images
-        real_A = Image.open(self.files_A[index % len(self.files_A)])
-        real_B = Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)])
-        # Normalize
-        if self.normalize:
-            real_A = (real_A - IMAGENET_MEAN) / IMAGENET_STD
-            real_B = (real_B - IMAGENET_MEAN) / IMAGENET_STD
+        real_A = Image.open(self.files_A[index % len(self.files_A)]).convert("RGB")
+        real_B = Image.open(
+            self.files_B[random.randint(0, len(self.files_B) - 1)]
+        ).convert("RGB")
         # Transform
-        real_A = self.transform(image=real_A.copy())
-        real_B = self.transform(image=real_B.copy())
-        # To Tensor
-        real_A = self.to_tensor(real_A.astype(np.float32))
-        real_B = self.to_tensor(real_B.astype(np.float32))
+        real_A = self.transform(real_A)
+        real_B = self.transform(real_B)
 
         return real_A, real_B
 
